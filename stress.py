008a4f1e1aae35453fd051b0b484a84dd2ec95ad
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-01-01 14:02:09 -0800

    only 10k requests

--- stress.py
+++ stress.py
@@ -8,12 +8,11 @@ import requests
 
 # send a request
 def send_request(url):
-    while True:
-        requests.get(url)
+    requests.get(url)
 
 if __name__ == "__main__":
     conn_pool = []
-    url = input("what url do you want to send to")
+    url = input("what url do you want to send to: ")
     for i in range(10000):
         print(i)
         Process(target=send_request, args=(url,)).start()
