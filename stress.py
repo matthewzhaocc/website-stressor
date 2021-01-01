570f82e0d3deaeafaa48868b2c579d282a642aa0
Matthew Zhao <matthew@mail.matthew-cloud.com>
2021-01-01 13:59:10 -0800

    done

---
+++ stress.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    A web stressor to test your infrastructure
+"""
+
+from multiprocessing import Process
+import requests
+
+# send a request
+def send_request(url):
+    while True:
+        requests.get(url)
+
+if __name__ == "__main__":
+    conn_pool = []
+    url = input("what url do you want to send to")
+    for i in range(10000):
+        print(i)
+        Process(target=send_request, args=(url,)).start()
